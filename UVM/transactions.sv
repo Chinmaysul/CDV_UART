3ed5a4bd09c7c0224ef9b107b46b7610f78739ff
Chinmaysul <90552831+Chinmaysul@users.noreply.github.com>
2025-02-07 17:11:25 +0530

    Add files via upload

---
+++ UVM/transactions.sv
@@ -0,0 +1,24 @@
+`timescale 1 ps/ 1 ps
+
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+
+class uart_trans extends uvm_sequence_item;
+
+
+  `uvm_object_utils(uart_trans)
+
+  bit rx;
+  rand bit [7:0] tx_data_in;
+  bit start;
+  bit tx;
+  bit [7:0] rx_data_out;
+  bit tx_active;
+  bit done_tx;
+
+
+  function new (string name = "");
+    super.new(name);
+  endfunction
+
+endclass: uart_trans
