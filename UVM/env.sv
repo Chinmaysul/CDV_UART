491f7e5a2371a4d83a20ad12d5acab034422ebcd
Chinmaysul <90552831+Chinmaysul@users.noreply.github.com>
2025-02-15 16:40:13 +0530

    Update env.sv

--- UVM/env.sv
+++ UVM/env.sv
@@ -5,16 +5,21 @@ class uart_env extends uvm_env;
   `uvm_component_utils(uart_env)
 
   uart_agent agent;
+  uart_scoreboard scb;
 
   function new(string name = "", uvm_component parent);
     super.new(name, parent);
   endfunction
 
   function void build_phase(uvm_phase phase);
-    agent = uart_agent::type_id::create("agent",this);  
+    agent = uart_agent::type_id::create("agent",this); 
+    scb = uart_scoreboard::type_id::create("scb",this);
+  endfunction
+  
+  function void connect_phase(uvm_phase phase);
+    agent.mon.ap_port.connect(scb.ap_export);
   endfunction
 
 
 endclass: uart_env
   
- 
